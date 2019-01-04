8615fe7f4a9e6deb6844bc987be7b73e586331ea
Sascha <TribuneX@users.noreply.github.com>
2019-01-04 11:18:16 +0100

    Add spring boot example app

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+group = "de.itemis"
+version = "0.0.1-SNAPSHOT"
+
+plugins {
+    val kotlinVersion = "1.3.11"
+    val springVersion = "2.1.1.RELEASE"
+    id("org.springframework.boot") version springVersion
+    id("org.jetbrains.kotlin.jvm") version kotlinVersion
+    id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
+    id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
+    id("io.spring.dependency-management") version "1.0.6.RELEASE"
+}
+
+repositories {
+    mavenCentral()
+}
+
+val test by tasks.getting(Test::class) {
+    useJUnitPlatform()
+}
+
+dependencies {
+    compile("org.jetbrains.kotlin:kotlin-stdlib") // Required for Kotlin integration
+    compile("org.springframework.boot:spring-boot-starter-web")
+    testCompile("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "junit")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
